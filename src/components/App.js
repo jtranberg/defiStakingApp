0b9f7267b25cff11fae8b73942aecbaf5f250d8a
jaytranberg <jtranberg@hotmail.ca>
2024-11-07 18:23:23 -0800

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,114 +1,126 @@
-import React, {Component} from 'react'
-import './App.css'
-import Navbar from './Navbar'
-import Web3 from 'web3'
-import Tether from '../truffle_abis/Tether.json'
-import RWD from '../truffle_abis/RWD.json'
-import DecentralBank from '../truffle_abis/DecentralBank.json'
-import Main1 from './Main1.js'
-// import ParticleSettings from './ParticleSettings.json'
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Web3 from 'web3';
+import Tether from '../truffle_abis/Tether.json';
+import RWD from '../truffle_abis/RWD.json';
+import DecentralBank from '../truffle_abis/DecentralBank.json';
+import Main1 from './Main1.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '0x0',
+      tether: {},
+      rwd: {},
+      decentralBank: {},
+      tetherBalance: '0',
+      rwdBalance: '0',
+      stakingBalance: '0',
+      loading: true,
+    };
+  }
 
-    async componentWillMount() {
-        await this.loadWeb3()
-        await this.loadBlockchainData()
+  async componentDidMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('No Ethereum browser detected. Please install MetaMask!');
     }
+  }
 
+  async loadBlockchainData() {
+    try {
+      const web3 = window.web3;
+      const accounts = await web3.eth.getAccounts();
+      this.setState({ account: accounts[0] });
 
-       async loadBlockchainData() {
-            const web3 = window.web3
-            const accounts = await web3.eth.getAccounts()
-            console.log(accounts)
-            this.setState({account:accounts[0]})
-            const networkId = await web3.eth.net.getId()
-           
-            // load tether contract
-            const tetherData = Tether.networks[networkId]
-              if(tetherData) {
-                const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
-                this.setState({tether})
-                 let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
-                 this.setState({tetherBalance: tetherBalance.toString()})
-              }else {
-                window.alert("tether contract not deplyed - network not deployed")
-              }
+      const networkId = await web3.eth.net.getId();
 
+      // Load Tether contract
+      const tetherData = Tether.networks[networkId];
+      let tetherBalance = '0';
+      let tether = {};
+      if (tetherData) {
+        tether = new web3.eth.Contract(Tether.abi, tetherData.address);
+        tetherBalance = await tether.methods.balanceOf(this.state.account).call();
+      }
 
-            const rwdData = RWD.networks[networkId]
-              if(rwdData) {
-                const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
-                this.setState({rwd})
-                 let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
-                 this.setState({rwdBalance: rwdBalance.toString() })
-              }else {
-                window.alert("rwd contract not deplyed - network not deployed")
-              }
+      // Load RWD contract
+      const rwdData = RWD.networks[networkId];
+      let rwdBalance = '0';
+      let rwd = {};
+      if (rwdData) {
+        rwd = new web3.eth.Contract(RWD.abi, rwdData.address);
+        rwdBalance = await rwd.methods.balanceOf(this.state.account).call();
+      }
 
+      // Load Decentral Bank contract
+      const decentralBankData = DecentralBank.networks[networkId];
+      let stakingBalance = '0';
+      let decentralBank = {};
+      if (decentralBankData) {
+        decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address);
+        stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call();
+      }
 
-            const decentralBankData = DecentralBank.networks[networkId]
-              if(decentralBankData) {
-                const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
-                this.setState({decentralBank})
-                 let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
-                 this.setState({stakingBalance: stakingBalance.toString() })
-                 }else {
-                window.alert("Decentral Bank contract not deplyed - network not deployed")
-              }
-
-              this.setState({loading:false})
-        }
-        
-        async loadWeb3() {
-        if(window.ethereum){
-            window.web3 = new Web3(window.ethereum)
-            await window.ethereum.enable()
-        } else if(window.web3) {
-            window.web3 =new Web3(window.web3.currentProvider)
-            } else {
-                window.alert('No Etheruem browser, check metamask!')
-            }
-        }
-
-    constructor(props) {
-       super(props)
-       this.state = {
-           account: '0x0',
-           tether:{},
-           rdw:{},
-           decenttralBank:{},
-           tetherBalance:'0',
-           rwdBalance:'0',
-           stakingBalance:'0',
-           loading: true
-        }
+      // Update state
+      this.setState({
+        tether,
+        rwd,
+        decentralBank,
+        tetherBalance: tetherBalance.toString(),
+        rwdBalance: rwdBalance.toString(),
+        stakingBalance: stakingBalance.toString(),
+        loading: false,
+      });
+    } catch (error) {
+      console.error('Error loading blockchain data:', error);
+      this.setState({ loading: false });
     }
-    // react code goes here
-    render() {
-      
-      // let content
-      // this.state.loading ?  content =
-      // <p id='loader' className='text-center' style={{margin:'30px'}}>
-      //  LOADING PLEASE ...</p> : content = 
-      //  <Main1 />
-        return(
-           <div>
-            <Navbar account={this.state.account}/>
-                <div className='container-fluid mt-5'>
-                  <div classname="row">
-                      <main role='main' className='col-lg-12 ml-auto mr-auto'style={{maxWidth: '600px', minHeight:'100vm'}}>
-                         <div>
-                         <Main1 tetherBalance={this.state.tetherBalance}
-                                   rwdBalance={this.state.rwdBalance}
-                                   stakingBalance={this.state.stakingBalance}/>
-                         </div>
-                      </main>
-                  </div>
-                </div>
-           </div>
-           
+  }
 
-       )
-    }
+  render() {
+    const { account, tetherBalance, rwdBalance, stakingBalance, loading } = this.state;
+
+    return (
+      <div>
+        <Navbar account={account} />
+        <div className='container-fluid mt-5'>
+          <div className="row">
+            <main
+              role='main'
+              className='col-lg-12 ml-auto mr-auto'
+              style={{ maxWidth: '600px', minHeight: '100vh' }}
+            >
+              <div>
+                {loading ? (
+                  <p id='loader' className='text-center' style={{ margin: '30px' }}>
+                    LOADING, PLEASE WAIT...
+                  </p>
+                ) : (
+                  <Main1
+                    tetherBalance={tetherBalance}
+                    rwdBalance={rwdBalance}
+                    stakingBalance={stakingBalance}
+                  />
+                )}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default App;
